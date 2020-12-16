6c77d6fe6bcde0a2495bce51a38bd47a53065ce5
unknown <3470226242@qq.com>
2020-12-16 20:31:59 +0800

    text1210

---
+++ text1210/app/src/main/java/com/example/text1210/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.text1210;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
